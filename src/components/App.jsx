87595c083b623f918d4218e8fd47aa866abbf241
charivna <kate.dzhurinskaya@gmail.com>
2023-12-21 19:38:56 +0100

    fonts and markup main page

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,6 @@
+import { Main } from './Main/Main';
+import '../fonts.css';
+
 export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
+  return <Main />;
 };
