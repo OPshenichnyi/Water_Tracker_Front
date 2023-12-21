b17c5be3e74a9665ffdf9c9826f19c07a1dd76d8
Jakson2023 <125270235+Jakson2023@users.noreply.github.com>
2023-12-21 17:22:12 +0200

    add ruotes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,30 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Route, Routes } from "react-router-dom"
+
+import HomePage from "../pages/HomePage"
+import SignUpPage from "../pages/SignUpPage"
+import SigninPage from "../pages/SigninPage"
+import Main from "../pages/Main"
+import Layout from "./SharedLayout/Layout"
+import NotFoundPage from "pages/NotFoundPage"
+
+
+const App =()=>{
+    return (
+        <Routes>
+            
+            <Route path="/" element={<Layout/>}>
+            <Route index element={<Main />} />
+            <Route path="/register" element={<SignUpPage />} />    
+            <Route path="/login" element={<SigninPage />} />           
+            <Route path="/" element={<HomePage />} />
+            <Route path="*" element={<NotFoundPage />} />
+          
+        
+        </Route>
+            </Routes>
+    )
+}
+
+
+export default App
+
---
+++ src/components/SharedLayout/Layout.jsx
@@ -0,0 +1,20 @@
+import Header from "components/Header/Header"
+import {  Outlet } from "react-router-dom"
+
+
+
+
+
+const Layout = ()=>{
+    return(
+        <div>
+
+
+<Header/>
+
+
+<Outlet/>
+</div>
+)
+}
+export default Layout
