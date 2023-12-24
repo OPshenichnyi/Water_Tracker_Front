dd0a81854404a99fce8f4c1442daac607fd5ebd4
OPshenichnyi <106489713+OPshenichnyi@users.noreply.github.com>
2023-12-24 20:09:58 +0200

    fix bag

--- src/components/UserAuth/UserAuth.jsx
+++ src/components/UserAuth/UserAuth.jsx
@@ -1,29 +1,21 @@
-import { NavLink } from 'react-router-dom';
-import { Signin,Wrapper } from './UserAuth.styled';
+import { NavLink } from "react-router-dom";
+import { Signin, Wrapper } from "./UserAuth.styled";
 
-import sprite from '../../common/symbol-defs.svg';
-
+import sprite from "../../common/symbol-defs.svg";
 
 export const UserAuth = () => {
+  // const { isLoggedIn } = useAuth();
 
-
-// const { isLoggedIn } = useAuth();
-
-    return (
-    
-        <Wrapper>
-            <Signin>
+  return (
+    <Wrapper>
+      <Signin>
+        <NavLink to="/signin">Sign in</NavLink>
+      </Signin>
       <NavLink to="/signin">
-        Sign in
-                </NavLink>
-            </Signin>
-      <NavLink  to="/signin">
         <svg width={28} height={28}>
-            <use href={`${sprite}#SigninNoAuth`} />
-          </svg>
+          <use href={`${sprite}#SigninNoAuth`} />
+        </svg>
       </NavLink>
     </Wrapper>
-   
-)
-
-}
+  );
+};
--- src/components/UserAuth/UserAuth.styled.js
+++ src/components/UserAuth/UserAuth.styled.js
@@ -1,19 +1,15 @@
-import styled from 'styled-components';
-import color from '../../common/Variables';
-import { NavLink } from 'react-router-dom';
-
+import styled from "styled-components";
+import color from "../../common/Variables";
 
 export const Signin = styled.a`
-color: ${color.bluePrimary};
-margin-right:8px;
-text-align:center;
+  color: ${color.bluePrimary};
+  margin-right: 8px;
+  text-align: center;
 
-font-size: 16px;
- 
+  font-size: 16px;
 `;
 
 export const Wrapper = styled.div`
-  display:flex;
+  display: flex;
   align-items: center;
-
 `;
