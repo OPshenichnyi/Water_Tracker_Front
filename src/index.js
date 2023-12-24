9f7eb022c42b60b2c024159f4a84e29fd1529059
OPshenichnyi <106489713+OPshenichnyi@users.noreply.github.com>
2023-12-24 21:18:32 +0200

    Create Provider and Persist gate

--- src/index.js
+++ src/index.js
@@ -1,15 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
-import './index.css';
-import { GlobalStyle } from 'common/GlobalStyles';
-import App from 'components/App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import "./index.css";
+import { GlobalStyle } from "common/GlobalStyles";
+import App from "components/App";
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter basename="/Water_Tracker_Front">
-      <GlobalStyle />
-      <App />
-    </BrowserRouter>
+    <Provider>
+      <PersistGate loading={null}>
+        <BrowserRouter basename="/Water_Tracker_Front">
+          <GlobalStyle />
+          <App />
+        </BrowserRouter>
+      </PersistGate>
+    </Provider>
   </React.StrictMode>
 );
