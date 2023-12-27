0a86cf89fc623b43435e374d2c9fd6199758fdff
OPshenichnyi <106489713+OPshenichnyi@users.noreply.github.com>
2023-12-27 17:38:00 +0200

    Base name

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter>
+        <BrowserRouter basename="/Water_Tracker_Front">
           <GlobalStyle />
           <App />
         </BrowserRouter>
@@ -21,4 +21,3 @@ ReactDOM.createRoot(document.getElementById("root")).render(
     </Provider>
   </React.StrictMode>
 );
-// basename="/Water_Tracker_Front"
