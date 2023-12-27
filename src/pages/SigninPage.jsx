b353750c3c417ac863fe09200e7b61e1e406bea8
makuusik <makunka44@gmail.com>
2023-12-27 02:09:04 +0100

    update 4

--- src/pages/SigninPage.jsx
+++ src/pages/SigninPage.jsx
@@ -1,7 +1,9 @@
+import React from 'react';
+import { Formik, Field, Form } from 'formik';
+
 import {
   H2,
   SingInLabel,
-  SingInInput,
   SingInButton,
   SingInButton2,
   SingInContainer,
@@ -9,44 +11,57 @@ import {
   SignInButtonContainer,
   Container,
   SingInSvg,
+  SingInInput,
+  Image,
 } from './SigninPageStyled';
-
+import DesctopBottle from '../images/DesktopBottle.png';
 import icons from '../common/symbol-defs.svg';
 
 const SignIn = () => {
   return (
     <Container>
+      <Image src={DesctopBottle} alt="" />
+
       <SingInContainer>
-        <H2>Sing In</H2>
+        <H2>Sign In</H2>
         <SignInFormContainer>
-          <SingInLabel>
-            Enter your email
-            <SingInInput
-              type="mail"
-              name="Email"
-              id="email"
-              placeholder="E-mail"
-            />
-          </SingInLabel>
-          <SingInLabel>
-            Enter your password
-            <SingInSvg width={16} height={16}>
-              <use href={`${icons}#eye`} />
-            </SingInSvg>
-            <SingInInput
-              type="password"
-              name="password"
-              id="password"
-              placeholder="Password"
-            />
-          </SingInLabel>
+          <Formik>
+            <Form>
+              <SingInLabel>
+                Enter your email
+                <Field
+                  type="mail"
+                  id="email"
+                  name="email"
+                  placeholder="E-mail"
+                  as={SingInInput}
+                />
+              </SingInLabel>
+
+              <SingInLabel>
+                Enter your password
+                <SingInSvg width={16} height={16}>
+                  <use href={`${icons}#eye`} />
+                </SingInSvg>
+                <Field
+                  type="password"
+                  id="password"
+                  name="password"
+                  placeholder="Password"
+                  as={SingInInput}
+                />
+              </SingInLabel>
+
+              <SignInButtonContainer>
+                <SingInButton type="submit">Sign In</SingInButton>
+                <SingInButton2 href="./SignUp">Sign Up</SingInButton2>
+              </SignInButtonContainer>
+            </Form>
+          </Formik>
         </SignInFormContainer>
-        <SignInButtonContainer>
-          <SingInButton type="button">Sing In</SingInButton>
-          <SingInButton2 type="button">Sing Up</SingInButton2>
-        </SignInButtonContainer>
       </SingInContainer>
     </Container>
   );
 };
+
 export default SignIn;
