07f285e00f986144514f080a5b9af9b852c2c259
Kirichenko14031991 <124730976+Kirichenko14031991@users.noreply.github.com>
2023-12-28 20:16:36 +0200

    Update

--- src/components/UserLogoutModal/UserLogoutModal.jsx
+++ src/components/UserLogoutModal/UserLogoutModal.jsx
@@ -1,39 +1,42 @@
-import React, { useState } from 'react';
-import Box from '@mui/material/Box';
-import Button from '@mui/material/Button';
-import Modal from '@mui/material/Modal';
-import { style, ButtonCancel, ButtonLogout, titleStyle, descriptionStyle } from './UserLogoutModal.styles';
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { logOut } from "../../redux/auth/authOperationApi";
+import { CancelButton, Container, CloseButton, Close , LogOutButton, LogOutContainer, LogOutText, LogOutTextSecond, ButtonsContainer, Modal, Overlay,  } from "./UserLogoutModal.styles";
 
-const UserLogoutModal = ({ onClose }) => {
-  const [open, setOpen] = useState(false);
+ const LogOut = ({ backdropClick, close }) => {
 
-  const handleOpen = () => setOpen(true);
-  const handleClose = () => {
-    setOpen(false);
-    onClose();
-  };
+    const dispatch = useDispatch();
+    
+    useEffect(() => {
+      const handleKeydown = e => {
+    if (e.code === 'Escape') {
+        close()
+      }
+    }
 
-  const handleLogout = () => {
-    handleClose();
-  };
+    window.addEventListener('keydown', handleKeydown)
+    
+    return () => {
+    window.removeEventListener('keydown', handleKeydown)
+    }
+}, [close])
 
-  return (
-    <div>
-      <Button onClick={handleOpen}>Open modal</Button>
-      <Modal open={open} onClose={handleClose}>
-        <Box sx={style}>
-          <h2 id="modal-title" style={titleStyle}>
-            Log out
-          </h2>
-          <p id="modal-description" style={descriptionStyle}>
-            Do you really want to leave?
-          </p>
-          <ButtonCancel onClick={handleClose}>Cancel</ButtonCancel>
-          <ButtonLogout onClick={handleLogout}>Logout</ButtonLogout>
-        </Box>
-      </Modal>
-    </div>
-  );
-};
-
-export default UserLogoutModal;
+    return (
+        <Overlay id="logout" onClick={backdropClick}>
+            <Modal>
+                <Container>
+                <LogOutContainer>
+                    <LogOutText>Log out</LogOutText>
+                    <CloseButton type="button" onClick={close}><Close /></CloseButton>
+                </LogOutContainer>
+                    <LogOutTextSecond>Do you really want to leave?</LogOutTextSecond>
+                    <ButtonsContainer>
+                        <LogOutButton type="submit" onClick={() => {dispatch(logOut()); close()}}>Log out</LogOutButton>
+                    <CancelButton onClick={close}>Cancel</CancelButton>
+                    </ButtonsContainer>
+                </Container>
+            </Modal>
+        </Overlay>
+    )
+}
+export default LogOut;
