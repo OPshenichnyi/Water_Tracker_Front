60649933daeed2c644ee3996bb01ac6ccd64a0f6
Nikolas1790 <125148113+Nikolas1790@users.noreply.github.com>
2023-12-30 20:43:28 +0200

    redax-today

--- src/components/Calendar/Today/Today.jsx
+++ src/components/Calendar/Today/Today.jsx
@@ -13,39 +13,63 @@ import {
   TimeTableData,
   AddWaterButton,
 } from "./Today.styled";
+import React, { useEffect } from "react";
+
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchWaterDataToday } from "../../../redux/water/operations"
+import {FormatTime} from  "../FormatTime/FormatTime"
+
 
 const Today = () => {
+  const waterData = useSelector(state => state.water);
+  const dispatch = useDispatch();
+
+useEffect(() => {
+  const fetchData = async () => {
+    try {
+      await dispatch(fetchWaterDataToday());
+    } catch (error) {
+      console.error('Ошибка при получении данных о воде в useEffect:', error);
+    }
+  };
+
+  fetchData();
+}, [dispatch]);
+
   return (
     <TodayContainer>
       <TodayHeader>Today</TodayHeader>
       <TableWrapper>
         <TodayTable>
           <tbody>
-            <TableRow>
-              <TodayTableData>
-                <ImageWrapper>
-                  <svg width={26} height={26}>
-                    <use href={`${sprite}#cup`}></use>
-                  </svg>
-                </ImageWrapper>
-              </TodayTableData>
-              <TextTableData>750 ml</TextTableData>
-              <TimeTableData>14:00 PM</TimeTableData>
-              <TodayTableData>
-                <Button>
-                  <svg width={16} height={16}>
-                    <use href={`${sprite}#pencil-square`}></use>
-                  </svg>
-                </Button>
-              </TodayTableData>
-              <TodayTableData>
-                <TrashButton>
-                  <svg width={16} height={16}>
-                    <use href={`${sprite}#trash-can`}></use>
-                  </svg>
-                </TrashButton>
-              </TodayTableData>
-            </TableRow>
+            
+          {waterData.history.map((waterRecord) => (
+              <TableRow key={waterRecord._id}>
+                <TodayTableData>
+                  <ImageWrapper>
+                    <svg width={26} height={26}>
+                      <use href={`${sprite}#cup`}></use>
+                    </svg>
+                  </ImageWrapper>
+                </TodayTableData>
+                <TextTableData>{waterRecord.waterVolume} ml</TextTableData>
+                <TimeTableData>{FormatTime(waterRecord.date)}</TimeTableData>
+                <TodayTableData>
+                  <Button>
+                    <svg width={16} height={16}>
+                      <use href={`${sprite}#pencil-square`}></use>
+                    </svg>
+                  </Button>
+                </TodayTableData>
+                <TodayTableData>
+                  <TrashButton>
+                    <svg width={16} height={16}>
+                      <use href={`${sprite}#trash-can`}></use>
+                    </svg>
+                  </TrashButton>
+                </TodayTableData>
+              </TableRow>
+            ))}
           </tbody>
         </TodayTable>
         <AddWaterButton type="submit">+ Add water</AddWaterButton>
---
+++ src/redux/water/selector.js
@@ -0,0 +1,6 @@
+export const selectAddWaterVolume = state => state.water.data ;
+
+// export const selectUpdateWaterVolume = state => state.water.data ;
+
+// export const selectAddWaterVolume = state => state.water.data ;
+
