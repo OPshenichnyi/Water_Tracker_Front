cd296566b34d49e5ffab16363a8df94456d6d206
Nikolas1790 <125148113+Nikolas1790@users.noreply.github.com>
2024-01-08 11:46:34 +0200

    L

--- src/components/EditDailyNorma/EditDailyNorma.jsx
+++ src/components/EditDailyNorma/EditDailyNorma.jsx
@@ -103,7 +103,7 @@ function EditDailyNorma({ closeModal }) {
 
   const saveWater = () => {  
     if(inputValue === '') return toast.info('Fill in the column how much water you will drink');
-    if(inputValue >15) return toast.info('The maximum daily intake cannot exceed 15 l');
+    if(inputValue >15) return toast.info('The maximum daily intake cannot exceed 15 L');
     console.log(inputValue)
       dispatch(saveWaterRate(Number(inputValue)*1000));
       toast.success('Daily water intake changed 👍');
